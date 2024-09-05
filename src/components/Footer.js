7f5ddd388d8a702c51bd87988093113c022845bb
noamguterman <noamguterman@gmail.com>
2024-09-05 12:12:23 +0300

    updated fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
         <div className="footer-container">
             <a href="https://x.com/NoamGuterman" target="_blank">
-            <i className="fa-brands fa-square-twitter"></i></a>
+            <i><FontAwesomeIcon icon={faSquareTwitter} /></i></a>
             <a href="https://www.facebook.com/nowamz" target="_blank">
-            <i className="fa-brands fa-square-facebook"></i></a>
+            <i><FontAwesomeIcon icon={faSquareFacebook} /></i></a>
             <a href="https://www.instagram.com/noamguterman" target="_blank">
-            <i className="fa-brands fa-square-instagram"></i></a>
+            <i><FontAwesomeIcon icon={faSquareInstagram} /></i></a>
             <a href="https://github.com/noamguterman" target="_blank">
-            <i className="fa-brands fa-square-github"></i></a>
+            <i><FontAwesomeIcon icon={faSquareGithub} /></i></a>
         </div>
     )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,8 @@
 import React from "react"
 import profilePic from '../assets/profile_pic.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Info() {
     return (
@@ -9,8 +12,8 @@ export default function Info() {
             <h3>Frontend Developer</h3>
             <h5><a href="https://www.noamguterman.com" target="_blank" className="website">noamguterman.com</a></h5>
             <div className="btns-container">
-                <a href="mailto: noamguterman@gmail.com" target="_blank"><button className="btn-email"><i className="fa-solid fa-envelope"></i> Email</button></a>
-                <a href="https://www.linkedin.com/in/noamguterman/" target="_blank"><button className="btn-linkedin"><i className="fa-brands fa-linkedin"></i> LinkedIn</button></a>
+                <a href="mailto: noamguterman@gmail.com" target="_blank"><button className="btn-email"><FontAwesomeIcon icon={faEnvelope} /> Email</button></a>
+                <a href="https://www.linkedin.com/in/noamguterman/" target="_blank"><button className="btn-linkedin"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</button></a>
             </div>
         </div>
     )
